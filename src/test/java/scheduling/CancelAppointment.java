7a6c8c98c72df9e67e850452d68c76f2337225ff
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2021-02-05 19:37:07 -0500

    Data Change+New Tests

--- src/test/java/scheduling/CancelAppointment.java
+++ src/test/java/scheduling/CancelAppointment.java
@@ -34,7 +34,7 @@ public class CancelAppointment extends base{
 		String clubId = prop.getProperty("X-Club4Id");
 		String bookId =  prop.getProperty("bookId");
 //		String dateTime = ReusableDates.getCurrentDate();
-		String dateTime = ReusableMethods.getTomorrowsDate();
+		String dateTime = ReusableMethods.getTomorrowsDate()+"T14:00:00";
 		
 		appointmentId = ReusableMethods.scheduleSingleMbrAppointment(companyId, customerId, clubId, itemId, bookId, dateTime);
 		
